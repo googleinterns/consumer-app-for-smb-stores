1d29c6057fa39abfb048befaf64f691f32dd9952
Ananya Jain <44133406+Ananyajain721@users.noreply.github.com>
2020-06-01 19:48:02 +0530

    Add the boilerplate VueJS setup along with the Authentication setup

---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="logout">Logout</button>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import firebase from "firebase";
+
+export default {
+  name: "Home",
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("login");
+        });
+    }
+  }
+};
+</script>
