0b537407e7e3f307431173646ea4be1265be037e
Ananya Jain <jainananya@google.com>
2020-06-01 18:20:03 +0530

    VueJS with firebase auth and changed author

---
+++ SMB_kirana_Auth/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import firebase from 'firebase'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '*',
+    redirect: '/login'
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentuser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (requiresAuth && !currentuser) next('login');
+  else if (!requiresAuth && currentuser) next('home');
+  else next();
+});
+
+export default router
